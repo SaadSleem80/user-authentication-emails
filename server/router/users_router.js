68822dff98ebc50dfd3b208166b3131d8ff28991
SaadSleem80 <saadsleem80@gmail.com>
2023-03-11 23:12:56 +0200

    fix email

--- server/router/users_router.js
+++ server/router/users_router.js
@@ -19,7 +19,7 @@ router.get(
   users_controller.content_get
 );
 
-router.get("/verifyEmail/:id", users_controller.verifyEmail_view);
+router.get("/verifyEmail", users_controller.verifyEmail_view);
 
 router.get("/verfiy/:token", users_controller.verifyEmail);
 
